d6313dc23e57028c8030581f363a7556f1fc99e5
Andy Lin <windz90@gmail.com>
2016-01-02 21:01:18 +0800

    update CustomActivity

--- app/src/main/java/com/flyingwing/base/view/CustomActivity.java
+++ app/src/main/java/com/flyingwing/base/view/CustomActivity.java
@@ -2,6 +2,7 @@ package com.flyingwing.base.view;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -19,15 +20,33 @@ public class CustomActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		Intent intent;
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			intent = new Intent(this, Main.class);
-			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-			startActivity(intent);
+			callNavigateUpTo();
 			break;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	public void callNavigateUpTo(){
+		String className = NavUtils.getParentActivityName(this);
+		if(getIntent().getAction() != null){
+			className = getIntent().getAction();
+		}
+		if(className != null){
+			Intent intent = new Intent();
+			intent.setClassName(this, className);
+			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+			NavUtils.navigateUpTo(this, intent);
+		}
+	}
+
+	public void customNavigateUpTo(String className){
+		Intent intent = new Intent();
+		intent.setClassName(this, className);
+		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+		startActivity(intent);
 	}
 }
