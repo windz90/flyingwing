3ea3f221f433b42d2d26a65b515c05b0625a9f62
Andy Lin <windz90@gmail.com>
2016-05-28 14:38:52 +0800

    update CustomRecyclerAdapter 1.0.9

--- app/src/main/java/com/flyingwing/base/widget/CustomRecyclerAdapter.java
+++ app/src/main/java/com/flyingwing/base/widget/CustomRecyclerAdapter.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2015 Andy Lin. All rights reserved.
- * @version 1.0.8
+ * @version 1.0.9
  * @author Andy Lin
  * @since JDK 1.5 and Android 2.2
  */
@@ -23,10 +23,11 @@ import com.flyingwing.base.util.Utils;
 
 import org.json.JSONArray;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "Convert2Diamond"})
 public abstract class CustomRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 	
 	protected OnItemClickListener mOnItemClickListener;
@@ -108,8 +109,14 @@ public abstract class CustomRecyclerAdapter extends RecyclerView.Adapter<Recycle
 	}
 
 	public void addRangeItemIn2DArray(String[][] stringArray, int positionStart){
-		if(m2DArray == null || stringArray == null || stringArray.length == 0 || m2DArray.length < positionStart){
+		if(stringArray == null || stringArray.length == 0){
 			return;
+		}
+		if((m2DArray == null ? 0 : m2DArray.length) < positionStart){
+			return;
+		}
+		if(m2DArray == null){
+			m2DArray = new String[0][0];
 		}
 		String[][] dataArrayNew = new String[m2DArray.length + stringArray.length][];
 		if(m2DArray.length == positionStart){
@@ -180,7 +187,13 @@ public abstract class CustomRecyclerAdapter extends RecyclerView.Adapter<Recycle
 	}
 
 	public void addRangeItemInComplexList(List<Map<String, ? super Object>> list, int positionStart){
-		if(mComplexList != null && mComplexList.size() >= positionStart){
+		if(mComplexList == null){
+			if(positionStart != 0){
+				return;
+			}
+			mComplexList = new ArrayList<Map<String, ? super Object>>();
+		}
+		if(mComplexList.size() >= positionStart){
 			int size = list.size();
 			for(int i=0; i<size; i++){
 				mComplexList.add(positionStart + i, list.get(i));
@@ -191,7 +204,13 @@ public abstract class CustomRecyclerAdapter extends RecyclerView.Adapter<Recycle
 	}
 
 	public void addItemInComplexList(Map<String, ? super Object> map, int index){
-		if(mComplexList != null && mComplexList.size() >= index){
+		if(mComplexList == null){
+			if(index != 0){
+				return;
+			}
+			mComplexList = new ArrayList<Map<String, ? super Object>>();
+		}
+		if(mComplexList.size() >= index){
 			mComplexList.add(index, map);
 			notifyItemRangeInserted(index, 1);
 			notifyItemRangeChanged(index, 1);
@@ -237,8 +256,14 @@ public abstract class CustomRecyclerAdapter extends RecyclerView.Adapter<Recycle
 		return mList;
 	}
 
-	public void addRangeItemInList(List<? super Object> list, int positionStart){
-		if(mList != null && mList.size() >= positionStart){
+	public void addRangeItemInList(List<?> list, int positionStart){
+		if(mList == null){
+			if(positionStart != 0){
+				return;
+			}
+			mList = new ArrayList<Object>();
+		}
+		if(mList.size() >= positionStart){
 			int size = list.size();
 			for(int i=0; i<size; i++){
 				mList.add(positionStart + i, list.get(i));
@@ -249,14 +274,20 @@ public abstract class CustomRecyclerAdapter extends RecyclerView.Adapter<Recycle
 	}
 
 	public void addItemInList(Object object, int index){
-		if(mList != null && mList.size() >= index){
+		if(mList == null){
+			if(index != 0){
+				return;
+			}
+			mList = new ArrayList<Object>();
+		}
+		if(mList.size() >= index){
 			mList.add(index, object);
 			notifyItemRangeInserted(index, 1);
 			notifyItemRangeChanged(index, 1);
 		}
 	}
 
-	public void updateRangeItemFormList(List<? super Object> list, int positionStart){
+	public void updateRangeItemFormList(List<?> list, int positionStart){
 		int size = list.size();
 		if(mList != null && mList.size() >= positionStart + size){
 			for (int i = 0; i < size; i++) {
@@ -296,7 +327,13 @@ public abstract class CustomRecyclerAdapter extends RecyclerView.Adapter<Recycle
 	}
 	
 	public void addRangeItemInJSONArray(JSONArray jsonArray, int positionStart){
-		if(mJsonArray != null && mJsonArray.length() >= positionStart){
+		if(mJsonArray == null){
+			if(positionStart != 0){
+				return;
+			}
+			mJsonArray = new JSONArray();
+		}
+		if(mJsonArray.length() >= positionStart){
 			int length = jsonArray.length();
 			for(int i=0; i<length; i++){
 				try {
@@ -309,13 +346,19 @@ public abstract class CustomRecyclerAdapter extends RecyclerView.Adapter<Recycle
 	}
 
 	public void addItemInJSONArray(Object object, int index){
-		if(mJsonArray != null && mJsonArray.length() >= index){
+		if(mJsonArray == null){
+			if(index != 0){
+				return;
+			}
+			mJsonArray = new JSONArray();
+		}
+		if(mJsonArray.length() >= index){
 			try {
 				mJsonArray.put(index, object);
 			} catch (Exception ignored) {}
+			notifyItemRangeInserted(index, 1);
+			notifyItemRangeChanged(index, 1);
 		}
-		notifyItemRangeInserted(index, 1);
-		notifyItemRangeChanged(index, 1);
 	}
 
 	public void updateRangeItemFormJSONArray(JSONArray jsonArray, int positionStart){
