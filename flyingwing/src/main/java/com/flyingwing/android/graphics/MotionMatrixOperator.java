f90f10488dde9c4408aef608fbafc16e28dab963
Andy Lin <windz90@gmail.com>
2019-06-23 16:22:00 +0800

    update and rename MotionMatrixOperator 3.0.0, split it to add TransformationGestureDetector 3.0.0

---
+++ flyingwing/src/main/java/com/flyingwing/android/graphics/MotionMatrixOperator.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2012 Andy Lin. All rights reserved.
+ * @version 3.0.0
+ * @author Andy Lin
+ * @since JDK 1.5 and Android 2.2
+ */
+
+package com.flyingwing.android.graphics;
+
+import android.graphics.Camera;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.widget.ImageView;
+
+@SuppressWarnings({"unused", "WeakerAccess"})
+public class MotionMatrixOperator {
+
+	public static final int AREA_INDEX_INVALID = -1;
+
+	private Matrix mMatrix, mMatrixByCamera;
+	private Camera mCamera;
+	private float[][] mAreasLTRB;
+	private float[] mMatrixValues;
+
+	public MotionMatrixOperator(){
+		mMatrix = new Matrix();
+	}
+
+	public MotionMatrixOperator(float[][] areasLTRB){
+		this();
+		setArea(areasLTRB);
+	}
+
+	public Matrix getMatrix(){
+		return mMatrix;
+	}
+
+	public void applyCanvas(Canvas canvas){
+		canvas.setMatrix(mMatrix);
+	}
+
+	public void applyImageView(ImageView imageView){
+		imageView.setImageMatrix(mMatrix);
+	}
+
+	public void applyImageViewAfterCorrection(ImageView imageView){
+		if(imageView.getScaleType() != ImageView.ScaleType.MATRIX){
+			imageView.setScaleType(ImageView.ScaleType.MATRIX);
+		}
+		imageView.setImageMatrix(mMatrix);
+	}
+
+	public void setArea(float[][] areasLTRB){
+		mAreasLTRB = areasLTRB;
+		if(mMatrixValues == null){
+			mMatrixValues = new float[9];
+		}
+	}
+
+	public float[][] getAreas(){
+		return mAreasLTRB;
+	}
+
+	public int getTapAreaIndex(float x, float y){
+		if(mAreasLTRB == null || mAreasLTRB.length == 0){
+			return AREA_INDEX_INVALID;
+		}
+		mMatrix.getValues(mMatrixValues);
+		for(int i = 0; i< mAreasLTRB.length; i++){
+			if(x >= mAreasLTRB[i][0] * mMatrixValues[Matrix.MSCALE_X] + mMatrixValues[Matrix.MTRANS_X]
+					&& y >= mAreasLTRB[i][1] * mMatrixValues[Matrix.MSCALE_Y] + mMatrixValues[Matrix.MTRANS_Y]
+					&& x <= mAreasLTRB[i][2] * mMatrixValues[Matrix.MSCALE_X] + mMatrixValues[Matrix.MTRANS_X]
+					&& y <= mAreasLTRB[i][3] * mMatrixValues[Matrix.MSCALE_Y] + mMatrixValues[Matrix.MTRANS_Y]){
+				return i;
+			}
+		}
+		return AREA_INDEX_INVALID;
+	}
+
+	public void translate(float xDifference, float yDifference){
+		mMatrix.postTranslate(xDifference, yDifference);
+	}
+
+	public void rotate(float angleDifference, float xCenter, float yCenter){
+		mMatrix.postRotate(angleDifference, xCenter, yCenter);
+	}
+
+	public void rotate(float angleDifference){
+		mMatrix.postRotate(angleDifference);
+	}
+
+	public void rotateByAxis(float angleDifference, int axisIndex, float xCenter, float yCenter){
+		if(mCamera == null){
+			mMatrixByCamera = new Matrix();
+			mCamera = new Camera();
+		}
+		mCamera.save();
+		switch (axisIndex) {
+			case 0:
+				mCamera.rotateX(angleDifference);
+				break;
+			case 1:
+				mCamera.rotateY(angleDifference);
+				break;
+			case 2:
+				mCamera.rotateZ(angleDifference);
+				break;
+		}
+		mCamera.getMatrix(mMatrixByCamera);
+		mCamera.restore();
+		mMatrixByCamera.preTranslate(-xCenter, -yCenter);
+		mMatrixByCamera.postTranslate(xCenter, yCenter);
+		mMatrix.postConcat(mMatrixByCamera);
+	}
+
+	public void rotateByAxis(float angleDifference, int axisIndex){
+		rotateByAxis(angleDifference, axisIndex, 0, 0);
+	}
+
+	public void scale(float scaleFactor, float xCenter, float yCenter){
+		mMatrix.postScale(scaleFactor, scaleFactor, xCenter, yCenter);
+	}
+
+	public void scale(float scaleFactor){
+		mMatrix.postScale(scaleFactor, scaleFactor);
+	}
+}
---
+++ flyingwing/src/main/java/com/flyingwing/android/view/TransformationGestureDetector.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2012 Andy Lin. All rights reserved.
+ * @version 3.0.0
+ * @author Andy Lin
+ * @since JDK 1.5 and Android 2.2
+ */
+
+package com.flyingwing.android.view;
+
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.view.ViewConfiguration;
+
+@SuppressWarnings("unused")
+public class TransformationGestureDetector {
+
+	public interface OnTransformationGestureListener {
+
+		void onTap(MotionEvent motionEvent, float xScreenCoordinate, float yScreenCoordinate);
+
+		void onTranslate(MotionEvent motionEvent, float xDifference, float yDifference, boolean isFling);
+
+		void onRotate(MotionEvent motionEvent, float angleDifference);
+
+		void onScale(MotionEvent motionEvent, float scaleFactor);
+	}
+
+	private OnTransformationGestureListener mOnTransformationGestureListener;
+	private VelocityTracker mVelocityTracker;
+	private final float[][] mPointsExisted = new float[2][2];
+	private final float[] mPointFirstDown = new float[2];
+	private final int[] mLocationOnScreen = new int[2];
+	private float mScale;
+
+	private int mMinGapDistance, mMinimumFlingVelocity, mMaximumFlingVelocity;
+
+	public TransformationGestureDetector(Context context, OnTransformationGestureListener onTransformationGestureListener) {
+		mOnTransformationGestureListener = onTransformationGestureListener;
+		if(context == null){
+			mMinGapDistance = ViewConfiguration.getTouchSlop();
+			mMinGapDistance *= mMinGapDistance;
+			mMinimumFlingVelocity = ViewConfiguration.getMinimumFlingVelocity();
+			mMaximumFlingVelocity = ViewConfiguration.getMaximumFlingVelocity();
+		}else{
+			ViewConfiguration viewConfiguration = ViewConfiguration.get(context);
+			mMinGapDistance = viewConfiguration.getScaledTouchSlop();
+			mMinGapDistance *= mMinGapDistance;
+			mMinimumFlingVelocity = viewConfiguration.getScaledMinimumFlingVelocity();
+			mMaximumFlingVelocity = viewConfiguration.getScaledMaximumFlingVelocity();
+		}
+	}
+
+	public boolean onTouchEvent(View view, MotionEvent motionEvent) {
+		int action = motionEvent.getAction();
+		view.getLocationOnScreen(mLocationOnScreen);
+		if(mVelocityTracker == null){
+			mVelocityTracker = VelocityTracker.obtain();
+		}
+		mVelocityTracker .addMovement(motionEvent);
+		boolean handled = false;
+
+		if(action == MotionEvent.ACTION_DOWN){
+			mPointFirstDown[0] = mPointsExisted[0][0] = motionEvent.getX(0) + mLocationOnScreen[0];
+			mPointFirstDown[1] = mPointsExisted[0][1] = motionEvent.getY(0) + mLocationOnScreen[1];
+			handled = true;
+		}else if((action & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_POINTER_UP && motionEvent.getPointerCount() == 2){
+			int actionIndex = motionEvent.getActionIndex();
+			int targetIndex = actionIndex == 0 ? actionIndex + 1 : 0;
+			mPointsExisted[0][0] = motionEvent.getX(targetIndex) + mLocationOnScreen[0];
+			mPointsExisted[0][1] = motionEvent.getY(targetIndex) + mLocationOnScreen[1];
+		}else if((action & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_POINTER_DOWN && motionEvent.getPointerCount() == 2){
+			mPointsExisted[0][0] = motionEvent.getX(0) + mLocationOnScreen[0];
+			mPointsExisted[0][1] = motionEvent.getY(0) + mLocationOnScreen[1];
+			mPointsExisted[1][0] = motionEvent.getX(1) + mLocationOnScreen[0];
+			mPointsExisted[1][1] = motionEvent.getY(1) + mLocationOnScreen[1];
+		}else if((action & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_POINTER_UP && motionEvent.getPointerCount() == 3){
+			int actionIndex = motionEvent.getActionIndex();
+			int targetIndex = actionIndex == 0 ? actionIndex + 1 : 0;
+			mPointsExisted[0][0] = motionEvent.getX(targetIndex) + mLocationOnScreen[0];
+			mPointsExisted[0][1] = motionEvent.getY(targetIndex) + mLocationOnScreen[1];
+			targetIndex = actionIndex == 2 ? actionIndex - 1 : 2;
+			mPointsExisted[1][0] = motionEvent.getX(targetIndex) + mLocationOnScreen[0];
+			mPointsExisted[1][1] = motionEvent.getY(targetIndex) + mLocationOnScreen[1];
+		}else if(action == MotionEvent.ACTION_MOVE){
+			float x1 = motionEvent.getX(0) + mLocationOnScreen[0];
+			float y1 = motionEvent.getY(0) + mLocationOnScreen[1];
+			float dx = x1 - mPointsExisted[0][0];
+			float dy = y1 - mPointsExisted[0][1];
+			mOnTransformationGestureListener.onTranslate(motionEvent, dx, dy, false);
+			if(motionEvent.getPointerCount() > 1){
+				float x2 = motionEvent.getX(1) + mLocationOnScreen[0];
+				float y2 = motionEvent.getY(1) + mLocationOnScreen[1];
+				float ax = mPointsExisted[1][0] - mPointsExisted[0][0];
+				float ay = mPointsExisted[1][1] - mPointsExisted[0][1];
+				float bx = x2 - x1;
+				float by = y2 - y1;
+				float dot = ax * bx + ay * by;
+				float cross = ax * by - bx * ay;
+				float distance1 = (float) Math.sqrt(ax * ax + ay * ay);
+				float distance2 = (float) Math.sqrt(bx * bx + by * by);
+				if(cross != 0){
+					float angle = (cross > 0 ? 1 : -1) * (float) (Math.acos(dot / (distance1 * distance2)) / Math.PI * 180);
+					// Avoid data gap caused by missing motion events
+					if(angle < 15 && angle > -15 && angle != 0){
+						mOnTransformationGestureListener.onRotate(motionEvent, angle);
+					}
+				}
+				float scale = distance2 / distance1;
+				// Avoid data gap caused by missing motion events
+				if(scale < 1.2f && scale > -0.8f && scale != 0){
+					mOnTransformationGestureListener.onScale(motionEvent, scale);
+				}
+				mPointsExisted[1][0] = x2;
+				mPointsExisted[1][1] = y2;
+			}
+			mPointsExisted[0][0] = x1;
+			mPointsExisted[0][1] = y1;
+		}else if(action == MotionEvent.ACTION_UP){
+			float x1 = motionEvent.getX(0) + mLocationOnScreen[0];
+			float y1 = motionEvent.getY(0) + mLocationOnScreen[1];
+			if(motionEvent.getEventTime() - motionEvent.getDownTime() <= ViewConfiguration.getTapTimeout()){
+				float dx = x1 - mPointFirstDown[0];
+				float dy = y1 - mPointFirstDown[1];
+				if(dx * dx + dy * dy <= mMinGapDistance){
+					mOnTransformationGestureListener.onTap(motionEvent, x1, y1);
+				}
+			}else{
+				mVelocityTracker.computeCurrentVelocity(1000, mMaximumFlingVelocity);
+				float velocityX = mVelocityTracker.getXVelocity(motionEvent.getPointerId(0));
+				float velocityY = mVelocityTracker.getYVelocity(motionEvent.getPointerId(0));
+				mVelocityTracker.recycle();
+				mVelocityTracker = null;
+				if(Math.abs(velocityX) > mMinimumFlingVelocity || Math.abs(velocityY) > mMinimumFlingVelocity){
+					mOnTransformationGestureListener.onTranslate(motionEvent, velocityX, velocityY, true);
+				}
+			}
+		}else if(action == MotionEvent.ACTION_CANCEL){
+			mVelocityTracker.recycle();
+			mVelocityTracker = null;
+		}
+		return handled;
+	}
+}
