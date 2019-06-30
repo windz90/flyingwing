d77e9eca925ce3c4bba2cf41bbf01a207fa27bc9
Andy Lin <windz90@gmail.com>
2019-06-30 18:56:59 +0800

    update NetworkUtils 1.1.0

--- flyingwing/src/main/java/com/flyingwing/android/net/NetworkUtils.java
+++ flyingwing/src/main/java/com/flyingwing/android/net/NetworkUtils.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2015 Andy Lin. All rights reserved.
- * @version 1.0.5
+ * @version 1.1.0
  * @author Andy Lin
  * @since JDK 1.5 and Android 2.2
  */
@@ -25,6 +25,7 @@ import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.DhcpInfo;
 import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiConfiguration;
@@ -55,51 +56,70 @@ import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
-@SuppressWarnings({"unused", "WeakerAccess", "Convert2Diamond", "UnusedReturnValue"})
+@SuppressWarnings({"unused", "WeakerAccess", "ForLoopReplaceableByForEach", "Convert2Diamond", "UnusedReturnValue"})
 public class NetworkUtils {
 
 	@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
-	public static boolean isAvailable(@NonNull Context context){
+	public static boolean isAvailableByInternet(@NonNull Context context){
 		ConnectivityManager connectivityManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 		if(connectivityManager == null){
 			return false;
+		}
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+			Network network = connectivityManager.getActiveNetwork();
+			if(network == null){
+				return false;
+			}
+			NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+			return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET);
 		}
 		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
 		return networkInfo != null && networkInfo.isAvailable();
 	}
 
+	@RequiresApi(api = Build.VERSION_CODES.M)
 	@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
-	public static boolean isConnected(@NonNull Context context){
+	public static boolean isAvailableByInternetForStrict(@NonNull Context context){
 		ConnectivityManager connectivityManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 		if(connectivityManager == null){
 			return false;
 		}
-		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-		return networkInfo != null && networkInfo.isConnected();
+		Network network = connectivityManager.getActiveNetwork();
+		if(network == null){
+			return false;
+		}
+		NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+		return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED);
 	}
 
 	@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
-	public static boolean isAnyAvailable(@NonNull Context context){
+	public static boolean isAnyAvailableByInternet(@NonNull Context context){
 		ConnectivityManager connectivityManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 		if(connectivityManager == null){
 			return false;
 		}
 		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
 			Network[] networks = connectivityManager.getAllNetworks();
+			NetworkCapabilities networkCapabilities;
+			for(int i=0; i<networks.length; i++){
+				networkCapabilities = connectivityManager.getNetworkCapabilities(networks[i]);
+				if(networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)){
+					return true;
+				}
+			}
 			NetworkInfo networkInfo;
-			for(Network network : networks){
-				networkInfo = connectivityManager.getNetworkInfo(network);
+			for(int i=0; i<networks.length; i++){
+				networkInfo = connectivityManager.getNetworkInfo(networks[i]);
 				if(networkInfo.isAvailable()){
 					return true;
 				}
 			}
 			return false;
 		}
-		//noinspection deprecation
 		NetworkInfo[] networkInfoArray = connectivityManager.getAllNetworkInfo();
 		if(networkInfoArray != null){
-			for(NetworkInfo networkInfo : networkInfoArray){
-				if(networkInfo.isAvailable()){
+			for(int i=0; i<networkInfoArray.length; i++){
+				if(networkInfoArray[i].isAvailable()){
 					return true;
 				}
 			}
@@ -107,85 +127,192 @@ public class NetworkUtils {
 		return false;
 	}
 
+	@RequiresApi(api = Build.VERSION_CODES.M)
 	@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
-	public static boolean isConnectedSelectedType(@NonNull Context context, int connectivityManagerType) {
+	public static boolean isAnyAvailableByInternetForStrict(@NonNull Context context){
 		ConnectivityManager connectivityManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 		if(connectivityManager == null){
 			return false;
 		}
-		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-		return networkInfo != null && networkInfo.isConnected() && networkInfo.getType() == connectivityManagerType;
+		Network[] networks = connectivityManager.getAllNetworks();
+		NetworkCapabilities networkCapabilities;
+		for(int i=0; i<networks.length; i++){
+			networkCapabilities = connectivityManager.getNetworkCapabilities(networks[i]);
+			if(networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
-	public static boolean isConnectedMobileNetwork(@NonNull Context context) {
-		return isConnectedSelectedType(context, ConnectivityManager.TYPE_MOBILE);
-	}
-
-	@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
-	public static boolean isConnectedWifi(@NonNull Context context) {
-		return isConnectedSelectedType(context, ConnectivityManager.TYPE_WIFI);
-	}
-
-	@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
-	public static boolean isConnectedFast(Context context){
+	public static boolean isConnectedByInternet(@NonNull Context context){
 		ConnectivityManager connectivityManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 		if(connectivityManager == null){
 			return false;
 		}
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+			Network network = connectivityManager.getActiveNetwork();
+			if(network == null){
+				return false;
+			}
+			NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+			return networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+					|| networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+					|| networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+					|| networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN);
+		}
 		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-		return networkInfo != null && networkInfo.isConnected() && isConnectedFast(networkInfo.getType(), networkInfo.getSubtype());
+		return networkInfo != null && networkInfo.isConnected();
+	}
+
+	@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+	@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
+	public static boolean isAnyConnectedByInternet(@NonNull Context context){
+		ConnectivityManager connectivityManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+		if(connectivityManager == null){
+			return false;
+		}
+		NetworkCapabilities networkCapabilities;
+		Network[] networks = connectivityManager.getAllNetworks();
+		for(int i=0; i<networks.length; i++){
+			networkCapabilities = connectivityManager.getNetworkCapabilities(networks[i]);
+			if(networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+					|| networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+					|| networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+					|| networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
+	public static boolean isConnectedByNetworkType(@NonNull Context context, int networkType) {
+		ConnectivityManager connectivityManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+		if(connectivityManager == null){
+			return false;
+		}
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+			Network network = connectivityManager.getActiveNetwork();
+			if(network == null){
+				return false;
+			}
+			NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+			return networkCapabilities.hasTransport(networkType);
+		}
+		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+		return networkInfo != null && networkInfo.isConnected() && networkInfo.getType() == networkType;
+	}
+
+	@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
+	public static boolean isConnectedByMobileNetwork(@NonNull Context context) {
+		return isConnectedByNetworkType(context, Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ? NetworkCapabilities.TRANSPORT_CELLULAR : ConnectivityManager.TYPE_MOBILE);
+	}
+
+	@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
+	public static boolean isConnectedByWifi(@NonNull Context context) {
+		return isConnectedByNetworkType(context, Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ? NetworkCapabilities.TRANSPORT_WIFI : ConnectivityManager.TYPE_WIFI);
+	}
+
+	@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+	@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
+	public static boolean isAnyConnectedByNetworkType(@NonNull Context context, int networkType) {
+		ConnectivityManager connectivityManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+		if(connectivityManager == null){
+			return false;
+		}
+		NetworkCapabilities networkCapabilities;
+		Network[] networks = connectivityManager.getAllNetworks();
+		for(int i=0; i<networks.length; i++){
+			networkCapabilities = connectivityManager.getNetworkCapabilities(networks[i]);
+			if(networkCapabilities.hasTransport(networkType)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+	@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
+	public static boolean isAnyConnectedByMobileNetwork(@NonNull Context context) {
+		return isAnyConnectedByNetworkType(context, NetworkCapabilities.TRANSPORT_CELLULAR);
+	}
+
+	@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+	@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
+	public static boolean isAnyConnectedByWifi(@NonNull Context context) {
+		return isAnyConnectedByNetworkType(context, NetworkCapabilities.TRANSPORT_WIFI);
 	}
 
 	/**
-	 * @param type networkInfo.getType()
-	 * @param subType networkInfo.getSubtype()
 	 * @return if connect speed 400 kbps up
 	 */
-	public static boolean isConnectedFast(int type, int subType){
-		if(type == ConnectivityManager.TYPE_WIFI){
-			return true;
+	@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
+	public static boolean isConnectedFastInternet(Context context){
+		ConnectivityManager connectivityManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+		if(connectivityManager == null){
+			return false;
 		}
-		if(type == ConnectivityManager.TYPE_MOBILE){
-			switch(subType){
-				case TelephonyManager.NETWORK_TYPE_1xRTT:
-					return false;// ~ 50-100 kbps
-				case TelephonyManager.NETWORK_TYPE_CDMA:
-					return false;// ~ 14-64 kbps
-				case TelephonyManager.NETWORK_TYPE_EDGE:
-					return false;// ~ 50-100 kbps
-				case TelephonyManager.NETWORK_TYPE_EVDO_0:
-					return true;// ~ 400-1000 kbps
-				case TelephonyManager.NETWORK_TYPE_EVDO_A:
-					return true;// ~ 600-1400 kbps
-				case TelephonyManager.NETWORK_TYPE_GPRS:
-					return false;// ~ 100 kbps
-				case TelephonyManager.NETWORK_TYPE_HSDPA:
-					return true;// ~ 2-14 Mbps
-				case TelephonyManager.NETWORK_TYPE_HSPA:
-					return true;// ~ 700-1700 kbps
-				case TelephonyManager.NETWORK_TYPE_HSUPA:
-					return true;// ~ 1-23 Mbps
-				case TelephonyManager.NETWORK_TYPE_UMTS:
-					return true;// ~ 400-7000 kbps
-				// Above API level 7, make sure to set android:targetSdkVersion to appropriate level to use these
-				case TelephonyManager.NETWORK_TYPE_EHRPD: // API level 11 
-					return true;// ~ 1-2 Mbps
-				case TelephonyManager.NETWORK_TYPE_EVDO_B: // API level 9
-					return true;// ~ 5 Mbps
-				case TelephonyManager.NETWORK_TYPE_HSPAP: // API level 13
-					return true;// ~ 10-20 Mbps
-				case TelephonyManager.NETWORK_TYPE_IDEN: // API level 8
-					return false;// ~25 kbps
-				case TelephonyManager.NETWORK_TYPE_LTE: // API level 11
-					return true;// ~ 10+ Mbps
-				// Unknown
-				case TelephonyManager.NETWORK_TYPE_UNKNOWN:
-				default:
-					return false;
+		NetworkInfo networkInfo;
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+			Network network = connectivityManager.getActiveNetwork();
+			if(network == null){
+				return false;
+			}
+			NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+			if(networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)){
+				return true;
+			}
+			if(!networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)){
+				return false;
+			}
+			networkInfo = connectivityManager.getNetworkInfo(network);
+			if(networkInfo == null){
+				return false;
+			}
+		}else{
+			networkInfo = connectivityManager.getActiveNetworkInfo();
+			if(networkInfo == null || !networkInfo.isConnected()){
+				return false;
+			}
+			int type = networkInfo.getType();
+			switch(type){
+				case ConnectivityManager.TYPE_WIFI:
+				case ConnectivityManager.TYPE_ETHERNET:
+					return true;
+			}
+			if(type != ConnectivityManager.TYPE_MOBILE){
+				return false;
 			}
 		}
-		return false;
+		switch(networkInfo.getSubtype()){
+			case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+			// 2G
+			case TelephonyManager.NETWORK_TYPE_GSM:
+			case TelephonyManager.NETWORK_TYPE_IDEN: // ~25 kbps, API level 8.
+			case TelephonyManager.NETWORK_TYPE_CDMA: // ~ 14-64 kbps
+			case TelephonyManager.NETWORK_TYPE_1xRTT: // ~ 50-100 kbps
+			case TelephonyManager.NETWORK_TYPE_EDGE: // ~ 50-100 kbps
+			case TelephonyManager.NETWORK_TYPE_GPRS: // ~ 100 kbps
+				return false;
+			// 3G
+			case TelephonyManager.NETWORK_TYPE_EVDO_0: // ~ 400-1000 kbps
+			case TelephonyManager.NETWORK_TYPE_EVDO_A: // ~ 600-1400 kbps
+			case TelephonyManager.NETWORK_TYPE_HSPA: // ~ 700-1700 kbps
+			case TelephonyManager.NETWORK_TYPE_EHRPD: // ~ 1-2 Mbps, API level 11.
+			case TelephonyManager.NETWORK_TYPE_UMTS: // ~ 400-7000 kbps
+			case TelephonyManager.NETWORK_TYPE_EVDO_B: // ~ 5 Mbps, API level 9.
+			case TelephonyManager.NETWORK_TYPE_HSDPA: // ~ 2-14 Mbps
+			case TelephonyManager.NETWORK_TYPE_HSUPA: // ~ 1-23 Mbps
+			case TelephonyManager.NETWORK_TYPE_HSPAP: // ~ 10-20 Mbps, API level 13.
+			case TelephonyManager.NETWORK_TYPE_TD_SCDMA:
+			// 4G
+			case TelephonyManager.NETWORK_TYPE_LTE: // ~ 10+ Mbps, API level 11.
+			case TelephonyManager.NETWORK_TYPE_IWLAN:
+			default:
+				return true;
+		}
 	}
 
 	/**
@@ -355,14 +482,14 @@ public class NetworkUtils {
 			}
 			return;
 		}
-		
+
 		/*
 		new BroadcastReceiver() {
 			@Override
 			public void onReceive(Context context, Intent intent) {
 				context.getApplicationContext().unregisterReceiver(this);
 				// <uses-permission android:name="android.permission.ACCESS_WIFI_STATE"/>
-				// <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/> or 
+				// <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/> or
 				// <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
 				List<ScanResult> list = wifiManager.getScanResults();
 			}
@@ -372,6 +499,7 @@ public class NetworkUtils {
 			context.getApplicationContext().registerReceiver(broadcastReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
 		}
 		// <uses-permission android:name="android.permission.CHANGE_WIFI_STATE"/>
+		// This method was deprecated in API level P.
 		wifiManager.startScan();
 	}
 
@@ -718,7 +846,7 @@ public class NetworkUtils {
 				if(isStartScan){
 					// <uses-permission android:name="android.permission.BLUETOOTH_ADMIN"/>
 					/*
-					<uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/> or 
+					<uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/> or
 					<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
 					*/
 					bluetoothLeScanner.startScan(scanCallback);
@@ -765,9 +893,7 @@ public class NetworkUtils {
 		// <uses-permission android:name="android.permission.BLUETOOTH"/>
 		if(bluetoothAdapter != null && bluetoothAdapter.isEnabled()){
 			// <uses-permission android:name="android.permission.BLUETOOTH_ADMIN"/>
-			//noinspection deprecation
 			bluetoothAdapter.stopLeScan(leScanCallback);
-			//noinspection deprecation
 			return isStartScan && bluetoothAdapter.startLeScan(leScanCallback);
 		}
 		return false;
@@ -1061,15 +1187,14 @@ public class NetworkUtils {
 	}
 
 	/**
-	 * 
 	 * android.permission.BLUETOOTH_ADMIN
 	 */
 	@RequiresPermission(android.Manifest.permission.BLUETOOTH_ADMIN)
 	public static boolean bluetoothPairCreateForQuiet(@NonNull Context context, BluetoothDevice bluetoothDevice, byte[] bytes, BroadcastReceiver broadcastReceiver
 			, Handler handlerNoPermissions){
-		return setBluetoothDevicePin(bluetoothDevice, bytes) && 
-		bluetoothPairCreate(context, bluetoothDevice, broadcastReceiver, handlerNoPermissions) && 
-		bluetoothCancelPairingUserInput(bluetoothDevice);
+		return setBluetoothDevicePin(bluetoothDevice, bytes)
+				&& bluetoothPairCreate(context, bluetoothDevice, broadcastReceiver, handlerNoPermissions)
+				&& bluetoothCancelPairingUserInput(bluetoothDevice);
 	}
 
 	/**
